9f40f5d65340b47255493b324c60797c6444d1fe
Angel Chaple <98296861+achaple0@users.noreply.github.com>
2025-07-15 14:13:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-todo_list = [""]
+todo_list = []
 
 running = True
 
@@ -8,22 +8,33 @@ def add_list_item(item):
 def delete_list_item(item):
      del todo_list[item]
 
+def view_list_item():
+    for index, item in enumerate(todo_list):
+        print(f"{index + 1}. {item}")
 
 # def update_list_item(): 
 #     return 
 
 while running: 
     user_selection = input("What would you like to do? ")
+    user_selection = user_selection.lower()
 
     if user_selection == "add":
         item = input("Enter your todo item: ")
         add_list_item(item)
-        print(todo_list)
-    if user_selection == "delete":
+    elif user_selection == "delete":
         item = input("Which item would you like to delete? Please enter the item number: ")
-        if item:
-            item = item.enumerate()
-            delete_list_item(item)
-        print(todo_list)
+        item = int(item)
+        item -= 1
+        if item >= int(len(todo_list)):
+            print("The item number you selected is greater than the length of the list that currently exists.")
+        for index, items in enumerate(todo_list):
+            if item == index:
+                delete_list_item(index)
+                view_list_item()
+    elif user_selection == "view":
+        view_list_item()
+    elif user_selection == "exit":
+        exit()
 
-    
+        
